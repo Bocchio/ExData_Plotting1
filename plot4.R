defa16924c620ffd8a3a0d3501dfd879ac64dcdb
Guido Bocchio <guido.bocchio@yahoo.com.ar>
2014-08-10 17:20:20 -0700

    More underscores

--- plot4.R
+++ plot4.R
@@ -29,7 +29,7 @@ plot(strptime(datetime, "%d/%m/%Y %H:%M:%S"), data_table$Sub_metering_1, xlab=""
 lines(strptime(datetime, "%d/%m/%Y %H:%M:%S"), data_table$Sub_metering_1)
 lines(strptime(datetime, "%d/%m/%Y %H:%M:%S"), data_table$Sub_metering_2, col="red")
 lines(strptime(datetime, "%d/%m/%Y %H:%M:%S"), data_table$Sub_metering_3,  col="blue")
-legend("topright", lty=c(1,1,1), col=c("black","red","blue"), bty="n", legend=c("Sub metering 1", "Sub metering 2", "Sub metering 3"), cex=.9)
+legend("topright", lty=c(1,1,1), col=c("black","red","blue"), bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=.9)
 plot(strptime(datetime, "%d/%m/%Y %H:%M:%S"), data_table$Global_reactive_power, ylab="Global_reactive_power", type="l", xlab="datetime")
 })
 # Close the png device
